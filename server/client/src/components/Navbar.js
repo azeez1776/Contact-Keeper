1c8a4c05c614434949cac45db4f039cdbf65c69a
azeez1776 <aboud3am81@gmail.com>
2021-10-01 02:17:32 +0300

    added the Navbar component

--- server/client/src/components/Navbar.js
+++ server/client/src/components/Navbar.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const Navbar = () => {
+const Navbar = ({ title, icon }) => {
     return (
-        <div>
-
+        <div className='navbar bg-primary'>
+            <h1>
+                <i className={icon} />{title}
+            </h1>
         </div>
     )
+}
+
+Navbar.proptype = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string,
+}
+
+Navbar.defaultProps = {
+    title: 'Contact Keeper',
+    icon: 'fas fa-id-card-alt'
 }
 
 export default Navbar
