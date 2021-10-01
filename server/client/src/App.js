75713654651a0dd8c752b7ffa8fb8634d35b9262
azeez1776 <aboud3am81@gmail.com>
2021-10-01 03:57:29 +0300

    imported react-router-dom and started using Route, Router and Switch

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import { Fragment } from 'react';
 
 const App = () => {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route
+              exact
+              path="/"
+              component={Home}
+            />
+            <Route
+              exact
+              path="/about"
+              component={About}
+            />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
   );
 }
 
